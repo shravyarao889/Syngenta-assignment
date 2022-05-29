059cf2402d27778a102e22ca3bdf2d0cac8d7419
shravyarao889 <96167495+shravyarao889@users.noreply.github.com>
2022-05-29 21:59:37 +0530

    done

--- Reactjs/src/components/empty.jsx
+++ Reactjs/src/components/empty.jsx
@@ -13,7 +13,7 @@ const Empty = () => {
     getRandomColor();
   }, []);
 
-  // generationg random colors
+ // generationg random colors
 
   const getRandomColor = () => {
     let color = [];
@@ -23,11 +23,33 @@ const Empty = () => {
     setBalloon(color);
   };
 
-  
+  // useEffect(() => {
+  //   let col = getColors();
+  //   setBalloon(col);
+  // }, []);
+
+  // const getColors = () => {
+  //   let arr = ["#8d5c21", "#cd92fa", "#fdff5f", "#80ffff", "#ee7fa8"];
+
+  //   let color = [];
+  //   for (let i = 0; i <5; i++) {
+  //     let index = getRandom(0, arr.length - 1);
+  //     color.push(arr[index]);
+  //     arr.splice(index, 1);
+  //   }
+  //   return color;
+  // };
+  // const getRandom = (min, max) => {
+  //   let step1 = max - min + 1;
+  //   let step2 = Math.random() * step1;
+  //   let result = Math.floor(step2) + min;
+  //   return result;
+  // };
 
   // shifting balloons into an empty div
 
   const handleShoot = () => {
+   
     let circle = balloon.find((el) => el[1] == input);
     if (!circle) {
       alert("not a valid no");
@@ -36,6 +58,7 @@ const Empty = () => {
     const ans = balloon.filter((el) => el[1] != input);
     setBalloon([...ans]);
     setShift([...shift, circle]);
+    
   };
 
   // sending back balloons to their original places
@@ -71,7 +94,7 @@ const Empty = () => {
           type="text"
           placeholder="Enter Balloon Number"
         />
-        <button className="button-64" role = "button" onClick={handleShoot}>
+        <button className="button-64" role="button" onClick={handleShoot}>
           <span class="text">Shoot</span>
         </button>
       </div>
